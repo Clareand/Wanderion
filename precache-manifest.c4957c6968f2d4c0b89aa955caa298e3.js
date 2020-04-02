fc9fcbe2c1e88cba576c81e5bcaab51eaeac0f3b
clareand <claraputrias@gmail.com>
2020-04-03 00:19:46 +0700

    Updates

---
+++ precache-manifest.c4957c6968f2d4c0b89aa955caa298e3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23e26080482c218142b90daa6462ff0",
+    "url": "/Wanderion/index.html"
+  },
+  {
+    "revision": "34aa7023a26b7d2bbd1c",
+    "url": "/Wanderion/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "126473fe2d1c90b05408",
+    "url": "/Wanderion/static/css/main.1144c280.chunk.css"
+  },
+  {
+    "revision": "34aa7023a26b7d2bbd1c",
+    "url": "/Wanderion/static/js/2.48f12264.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/Wanderion/static/js/2.48f12264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "126473fe2d1c90b05408",
+    "url": "/Wanderion/static/js/main.7debfbb3.chunk.js"
+  },
+  {
+    "revision": "71adac5e7f37f9df19df",
+    "url": "/Wanderion/static/js/runtime-main.78923e75.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Wanderion/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "1d583ddf00735caaba09788c8cb0c6c8",
+    "url": "/Wanderion/static/media/magic-wand.1d583ddf.png"
+  },
+  {
+    "revision": "3cc457d7dd24d73144821d3304a66cda",
+    "url": "/Wanderion/static/media/orion-hand.3cc457d7.jpg"
+  },
+  {
+    "revision": "860e021fa77032c8af4cdd73c4fca178",
+    "url": "/Wanderion/static/media/telescope.860e021f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Web3AP-ClaraPutri/precache-manifest.53c6708570c9e5a6a67a1f072ecd9a52.js"
+  "/Wanderion/precache-manifest.c4957c6968f2d4c0b89aa955caa298e3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Web3AP-ClaraPutri/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Wanderion/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
