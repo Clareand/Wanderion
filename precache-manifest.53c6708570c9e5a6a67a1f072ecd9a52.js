249b697aa67ec4a40af9d40ac111a50fbfdb8ff7
clareand <claraputrias@gmail.com>
2020-04-03 00:12:53 +0700

    Updates

---
+++ precache-manifest.53c6708570c9e5a6a67a1f072ecd9a52.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35928832d6a783d001ad254a816332fa",
+    "url": "/Web3AP-ClaraPutri/index.html"
+  },
+  {
+    "revision": "34aa7023a26b7d2bbd1c",
+    "url": "/Web3AP-ClaraPutri/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "222e8deb2233cefd94ec",
+    "url": "/Web3AP-ClaraPutri/static/css/main.1144c280.chunk.css"
+  },
+  {
+    "revision": "34aa7023a26b7d2bbd1c",
+    "url": "/Web3AP-ClaraPutri/static/js/2.48f12264.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/Web3AP-ClaraPutri/static/js/2.48f12264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "222e8deb2233cefd94ec",
+    "url": "/Web3AP-ClaraPutri/static/js/main.ac28f14b.chunk.js"
+  },
+  {
+    "revision": "c9564d44287df8886abf",
+    "url": "/Web3AP-ClaraPutri/static/js/runtime-main.5f8c6b02.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Web3AP-ClaraPutri/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "1d583ddf00735caaba09788c8cb0c6c8",
+    "url": "/Web3AP-ClaraPutri/static/media/magic-wand.1d583ddf.png"
+  },
+  {
+    "revision": "3cc457d7dd24d73144821d3304a66cda",
+    "url": "/Web3AP-ClaraPutri/static/media/orion-hand.3cc457d7.jpg"
+  },
+  {
+    "revision": "860e021fa77032c8af4cdd73c4fca178",
+    "url": "/Web3AP-ClaraPutri/static/media/telescope.860e021f.png"
+  }
+]);
