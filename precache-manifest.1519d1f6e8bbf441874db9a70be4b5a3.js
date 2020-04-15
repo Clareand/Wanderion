5b947374cdd938c575c561a82f2a2158b6ecce8b
clareand <claraputrias@gmail.com>
2020-04-15 15:55:51 +0700

    Updates

---
+++ precache-manifest.1519d1f6e8bbf441874db9a70be4b5a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "094b7e151c13f88b327dc09ff02d51a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae7c75866096d41254ad",
+    "url": "/static/css/main.42639e65.chunk.css"
+  },
+  {
+    "revision": "c78ab0a329b81f732279",
+    "url": "/static/js/2.f82a5119.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.f82a5119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae7c75866096d41254ad",
+    "url": "/static/js/main.d7a395d3.chunk.js"
+  },
+  {
+    "revision": "b8b5c808a6a82f4cc271",
+    "url": "/static/js/runtime-main.361d18d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
